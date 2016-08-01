24ee44f8313fb32e34479b67e4f0a9b082426c5b
guangyuj <593082537@163.com>
2016-08-01 21:46:17 +0800

    @
    @

--- src/com/atguigu/github/test/Test01.java
+++ src/com/atguigu/github/test/Test01.java
@@ -3,6 +3,6 @@ package com.atguigu.github.test;
 public class Test01 {
 	public static void main(String[] args) {
 		System.out.println("hello github服务器");
-		System.out.println("server 01");
+		System.out.println("server 01@");
 	}
 }
